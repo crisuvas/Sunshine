b4b6b8ab37b3dfb3ac894e07d55a797562a1208c
Cris <crisuvas@gmail.com>
2018-05-09 18:49:16 -0500

    Initial commit

---
+++ app/src/main/java/com/ruiz/cris/sunshine/ForecastActivity.kt
@@ -0,0 +1,12 @@
+package com.ruiz.cris.sunshine
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ForecastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast)
+    }
+}
